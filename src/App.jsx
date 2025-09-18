234cb3e112a24e853a32b18d20470f35957f09b0
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-18 23:15:48 +0600

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-
-import './App.css'
+import { Suspense } from "react";
+import "./App.css";
+import Countries from "./Countries";
+const countriesPromise = fetch("https://openapi.programming-hero.com/api/all")
+  .then((res) => res.json());
 
 function App() {
-  
-
   return (
     <>
       <h1>React on the gooooo</h1>
+      <Suspense fallback={<p>Nadir bhai loading...</p>}>
+        <Countries countriesPromise={countriesPromise}></Countries>
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
