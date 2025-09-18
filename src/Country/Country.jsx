4aa61946126449120041f173fb8bcf7dc459ccdb
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-09-19 01:16:15 +0600

    added css

--- src/Country/Country.jsx
+++ src/Country/Country.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import './Country.css';
 const Country = ({country}) => {
     console.log(country.name.common);
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt="" />
             <h3>Name: {country.name.common}</h3>
             <p>Population: {country.population.population}</p>
